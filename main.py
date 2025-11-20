1d8621c32e90950785a6377f715f34cfb7c9c848
deveshmundharikar <rooknighty@gmail.com>
2025-11-20 16:24:46 +0530

    first commit dvc

---
+++ main.py
@@ -0,0 +1,24 @@
+import pandas as pd 
+import os 
+#create a sample DataFreame with in cloumns name 
+
+data = {'name':['rooknight','luffy','zoro'],
+        'age':[21,22,23],
+        'city':['mumbai','pune','bangalore']}
+
+df = pd.DataFrame(data)
+print(df)
+ 
+
+
+#ensure the dara directory exists at the root level
+data_dir = 'data'
+os.makedirs(data_dir, exist_ok=True)
+
+#define the file path 
+file_path = os.path.join(data_dir, 'sample.csv')
+
+#save the dataframe to the csv files , including columns names 
+df.to_csv(file_path, index=False)
+
+print(f"csv files saved to {file_path}")
